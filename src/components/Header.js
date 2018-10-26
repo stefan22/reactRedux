792b645b5b5d59d2dfd557f2d0bd2168c63f2b1e
stefano1020 <stefano.pasquale@gmx.co.uk>
2018-10-26 23:15:00 +0000

    [Template] set initial template

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,22 @@ import { NavLink } from 'react-router-dom';
 
 const Header = () => (
   <header>
-    <h1>Expensify</h1>
-    <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
-    <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
-    <NavLink to="/help" activeClassName="is-active">Help</NavLink>
+    <h1>Expenses Report</h1>
+    <h3>Create, add, edit and delete your job expenses</h3>
+    <hr />
+
+    <nav>
+      <NavLink to="/" activeClassName="is-active" exact={true}>
+         Dashboard
+      </NavLink>
+      <NavLink to="/create" activeClassName="is-active">
+         Create Expense
+      </NavLink>
+      <NavLink to="/help" activeClassName="is-active">
+         Help
+      </NavLink>
+   </nav>
+
   </header>
 );
 
