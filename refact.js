bb56b376b730aadec3f1de4f85581ecd9aa9a529
Stefano L <stefan22@users.noreply.github.com>
2018-11-08 00:27:40 +0000

    Update refact.js

--- refact.js
+++ refact.js
@@ -1,4 +1,68 @@
 debugger;
+let translations,
+    matchKey = 'Betslip.Hello kitty',
+objTran = window.fdjGamingWidgetTranslations = {
+   "Betslip. You have no bets at the moment.": "No tienes ninguna apuesta",
+   "Betslip. Hello Guess": "Hola chico",
+   "Betslip.Hello kitty":"test one two three"
+};
+
+const objProps = {
+   values: 'values',
+   keys: 'keys',
+   fnValues: function() {
+      return Object.values(objTran);
+   },
+   fnKeys: function() {
+      return Object.keys(objTran);
+   }
+},
+
+findMatch = (obj) => {
+   let isTranslation;
+   for (var i=0; i < obj.length; i++) {
+      if(matchKey == obj[i]) {
+         isTranslation = objTran[matchKey];
+         return isTranslation;
+      }
+   }
+},
+
+getObjProp = (op) => {
+   let tranKeys, tranValues;
+   if(op == 'values') {
+      tranValues = objProps.fnValues();
+      translations = findMatch(tranValues);
+      return translations;
+   } else if(op == 'keys') {
+      tranKeys = objProps.fnKeys();
+      translations = findMatch(tranKeys);
+      console.log(' translations: \n ' + translations);
+      return translations;
+   }
+};
+
+getObjProp.call(objProps, 'keys');
+
+
+
+
+
+
+
+
+
+======================================================================
+
+
+
+
+
+
+
+
+
+debugger;
 objTran = window.fdjGamingWidgetTranslations = {
    "Betslip. You have no bets at the moment.": "No tienes ninguna apuesta",
    "Betslip. Hello Guess": "Hola chico"
