e60dbf825ce5d97d0a5118eaebadbd244bda3022
Stefano L <stefan22@users.noreply.github.com>
2018-11-20 18:39:21 +0000

    Update compare.js

--- migratingToWebpack4/webpack/compare.js
+++ migratingToWebpack4/webpack/compare.js
@@ -196,3 +196,113 @@ module.exports = {
     jsonpFunction: 'fdjgsJsonp',
   },
 };
+
+
+------------
+{
+  "name": "fdj-gaming-widgets",
+  "version": "1.4.11",
+  "description": "FDJ Gaming Solution (UK) widgets",
+  "private": true,
+  "author": {
+    "name": "Zircode Ltd",
+    "email": "hello@zircode.com",
+    "url": "https://zircode.com/"
+  },
+  "repository": {
+    "type": "git",
+    "url": "ssh://git@bitbucket.lvs.co.uk/widg/widgets.git"
+  },
+  "main": "index.js",
+  "scripts": {
+    "start": "NODE_ENV=development ./node_modules/webpack-dev-server/bin/webpack-dev-server.js",
+    "build": "./node_modules/.bin/webpack -p --config webpack.config.js",
+    "test": "./node_modules/eslint/bin/eslint.js --ext .jsx --ext .js . && npm audit && NODE_ENV=development ./node_modules/karma/bin/karma start && ./node_modules/jest/bin/jest.js",
+    "format": "./node_modules/eslint/bin/eslint.js --ext .jsx --ext .js --fix .",
+    "watch": "./node_modules/.bin/webpack --watch --config webpack.config.js"
+  },
+  "dependencies": {
+    "babel-core": "^6.26.3",
+    "babel-plugin-array-includes": "^2.0.3",
+    "babel-plugin-transform-runtime": "^6.23.0",
+    "babel-preset-react": "^6.24.1",
+    "babel-runtime": "^6.26.0",
+    "capitalize": "^1.0.0",
+    "classnames": "^2.2.5",
+    "enzyme": "^3.3.0",
+    "enzyme-adapter-react-16": "^1.1.1",
+    "eslint": "^3.19.0",
+    "eslint-config": "^0.3.0",
+    "eslint-config-airbnb": "^15.1.0",
+    "eslint-plugin-import": "^2.9.0",
+    "eslint-plugin-jsx-a11y": "^5.0.1",
+    "eslint-plugin-react": "^7.7.0",
+    "form-serialize": "^0.7.2",
+    "handlebars": "^4.0.11",
+    "handlebars-loader": "^1.6.0",
+    "hawk": "^7.0.7",
+    "humanize-string": "^1.0.2",
+    "jasmine": "^2.99.0",
+    "jasmine-core": "^2.99.1",
+    "jasmine-promises": "^0.4.1",
+    "jest": "^23.6.0",
+    "jest-cli": "^23.6.0",
+    "jquery": "^3.3.1",
+    "karma": "^3.0.0",
+    "karma-chrome-launcher": "^2.2.0",
+    "karma-jasmine": "^1.1.1",
+    "karma-webpack": "^2.0.13",
+    "moment": "^2.21.0",
+    "moment-timezone": "^0.5.21",
+    "numeral": "^2.0.6",
+    "prop-types": "^15.6.1",
+    "react": "^16.2.0",
+    "react-dom": "^16.2.0",
+    "react-responsive": "^1.3.4",
+    "react-router": "^4.2.0",
+    "react-router-dom": "^4.2.2",
+    "react-test-renderer": "^16.2.0",
+    "react-transition-group": "^1.2.1",
+    "uuid": "^3.2.1",
+    "webpack": "^4.25.1",
+    "webpack-dev-middleware": "^3.4.0",
+    "webpack-dev-server": "^3.1.10"
+  },
+  "jest": {
+    "setupFiles": [
+      "./utils/jest.shim.js",
+      "./jest.config.jsx"
+    ],
+    "snapshotSerializers": [
+      "jest-serializer-html-string"
+    ]
+  },
+  "engines": {
+    "node": ">=8.1.0"
+  },
+  "os": [
+    "!win32"
+  ],
+  "devDependencies": {
+    "ajv-errors": "^1.0.0",
+    "babel-loader": "^7.1.5",
+    "babel-preset-env": "^1.7.0",
+    "clean-webpack-plugin": "^1.0.0",
+    "compression-webpack-plugin": "^2.0.0",
+    "css-loader": "^1.0.1",
+    "enzyme-to-json": "^3.3.4",
+    "file-loader": "^2.0.0",
+    "html-webpack-exclude-assets-plugin": "0.0.7",
+    "html-webpack-plugin": "^3.2.0",
+    "jest-serializer-html-string": "^1.0.1",
+    "mini-css-extract-plugin": "^0.4.4",
+    "node-sass": "^4.10.0",
+    "npm-install-webpack-plugin": "^4.0.5",
+    "sass-loader": "^7.1.0",
+    "style-loader": "^0.23.1",
+    "url-loader": "^1.1.2",
+    "webpack-cli": "^3.1.2",
+    "webpack-core": "^0.6.9"
+  }
+}
+
